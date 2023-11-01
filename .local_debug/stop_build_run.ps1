809f265c91530e3187bdd1ee075934700883a555
Alberto Geniola <albertogeniola@gmail.com>
2023-11-01 09:51:59 +0100

    Updated rebuild script

--- .local_debug/stop_build_run.ps1
+++ .local_debug/stop_build_run.ps1
@@ -1,4 +1,3 @@
-$container_id=docker ps --filter name=meross_local_broker --format "{{.ID}}"
-docker stop $container_id
-& .local_debug\build_addon_locally.ps1
-& .local_debug\run_addon_locally.ps1
+docker stop meross_local_broker
+.local_debug\build_addon_locally.ps1
+.local_debug\run_addon_locally.ps1
